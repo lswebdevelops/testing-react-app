62975d8f5f0b70bb208af7d17d587d8b066ff757
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-07-13 16:25:09 -0300

    first test

---
+++ src/components/__tests__/todo.test.js
@@ -0,0 +1,10 @@
+import { render, screen, cleanup } from '@testing-library/react'
+import Todo from '../todo';
+
+test('should render todo component', () => {
+     render(<Todo />)
+     const todoElement = screen.getByTestId('todo-1');
+     expect(todoElement).toBeInTheDocument();
+
+
+})
