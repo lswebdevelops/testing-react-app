aac7f582ad108422a1efc5c7ccf46075b7cf1a29
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-07-14 10:34:53 -0300

    testing restart and switch buttons

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,42 +1,39 @@
 import React, { useState } from "react";
 
 function Counter({ initialCount }) {
-    const [ count , setCount ] = useState(initialCount);
-
+    const [count, setCount] = useState(initialCount);
+  
     const increment = () => {
-        setCount((prev) => prev + 1 );
+      setCount((prev) => prev + 1);
     };
-
-    const decrement =() => {
-        setCount((prev) => prev - 1 );
-
+  
+    const decrement = () => {
+      setCount((prev) => prev - 1);
     };
-
+  
     const restart = () => {
-
-        setCount(0);
+      setCount(0);
     };
 
-    const switchSigns = () => {
-        setCount((prev) => prev * -1 );
+  const switchSigns = () => {
+    setCount((prev) => prev * -1);
+  };
 
-    };
-
-    return(
-        <div>
-            <h1>
-                Count: <h3 data-testid="count" >{count}</h3>
-            </h1>
-        <div>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
-            <button onClick={restart}>Restart</button>
-            <button onClick={switchSigns}>Switch Signs</button>
-        </div>
-        </div>
-
-    );
-    
-};
+  return (
+    <div>
+      <header>
+        <h1>Counter</h1>
+      </header>
+      <main>
+        <h2>Count:</h2>
+        <h3 data-testid="count">{count}</h3>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={restart}>Restart</button>
+        <button onClick={switchSigns}>Switch Signs</button>
+      </main>
+    </div>
+  );
+}
 
 export default Counter;
--- src/components/__tests__/Counter.test.jsx
+++ src/components/__tests__/Counter.test.jsx
@@ -5,44 +5,63 @@ describe(Counter, () => {
   it("counter displays correct initial count", () => {
     render(<Counter initialCount={0} />);
     const countValue = Number(screen.getByTestId("count").textContent);
-    expect(countValue).toEqual(0);
+    expect(countValue).toBe(0);
   });
 
   it("count should increment by 1 if the increment button is clicked", () => {
     // we have to access the button so we can test it
-    const { getByTestId, betByRole } = render(<Counter initialCount={0} />);
+    const { getByTestId, getByRole } = render(<Counter initialCount={0} />);
     const incrementBtn = screen.getByRole("button", { name: "Increment" });
     // before click happens
     const countValue0 = Number(screen.getByTestId("count").textContent);
-    expect(countValue0).toEqual(0);
+    expect(countValue0).toBe(0);
     // after click happens
     fireEvent.click(incrementBtn);
     const countValue1 = Number(screen.getByTestId("count").textContent);
-    expect(countValue1).toEqual(1);
+    expect(countValue1).toBe(1);
     // after click happens twice
     fireEvent.click(incrementBtn);
     const countValue2 = Number(screen.getByTestId("count").textContent);
-    expect(countValue2).toEqual(2);
+    expect(countValue2).toBe(2);
   });
-
 
   it("count should decrement by 1 if the decrement button is clicked", () => {
     // we have to access the button so we can test it
-    const { getByTestId, betByRole } = render(<Counter initialCount={0} />);
+    const { getByTestId, getByRole } = render(<Counter initialCount={0} />);
     const decrementBtn = screen.getByRole("button", { name: "Decrement" });
     // before click happens
     const countValue0 = Number(screen.getByTestId("count").textContent);
-    expect(countValue0).toEqual(0);
+    expect(countValue0).toBe(0);
     // after click happens
     fireEvent.click(decrementBtn);
     const countValue1 = Number(screen.getByTestId("count").textContent);
-    expect(countValue1).toEqual(-1);
+    expect(countValue1).toBe(-1);
     // after click happens twice
     fireEvent.click(decrementBtn);
     const countValue2 = Number(screen.getByTestId("count").textContent);
-    expect(countValue2).toEqual(-2);
+    expect(countValue2).toBe(-2);
+  });
+  
+  
+  it("Count should be 0 if the restart button is clicked", () => {
+    const { getByTestId, getByRole } = render(<Counter initialCount={50} />);
+    // before click:
+    const restartBtn = screen.getByRole("button", { name: "Restart" });
+    expect(Number(screen.getByTestId("count").textContent)).toEqual(50);
+    // after click:
+    fireEvent.click(restartBtn);
+    expect(Number(screen.getByTestId("count").textContent)).toEqual(0);
   });
 
-  
+
+  it("Count should invert signs if Switch signs button is clicked", () => {
+    const { getByTestId, getByRole } = render(<Counter initialCount={5} />);
+    // before click:
+    const switchBtn = screen.getByRole("button", { name: "Switch Signs" });
+    expect(Number(screen.getByTestId("count").textContent)).toBe(5);
+    // after click:
+    fireEvent.click(switchBtn);
+    expect(Number(screen.getByTestId("count").textContent)).toBe(-5);
+  });
 
 });
